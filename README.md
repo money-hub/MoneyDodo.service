8114dd7063b1c9e76a18eb4979937ae3df2e7c2c
liuyh73 <15989067460@163.com>
2019-04-16 18:17:53 +0800

    add data.sql & Update README

---
+++ README.md
@@ -0,0 +1,44 @@
+# MoneyDodo.service
+
+此项目为MoneyDodo.web和MoneyDodo.wechat的后台服务实现，采用微服务，使用go-kit框架，代码主体利用[GoKit CLI](<https://github.com/kujtimiihoxha/kit>)开源工具快速生成，便于将开发重心放到业务逻辑上。
+
+## 代码结构介绍
+
+**conf**
+
+存放数据库配置文件，具体配置方法参考`conf.example.yml`
+
+**db**
+
+存放数据库相关内容，初始化xorm、MySQL引擎
+
+**model**
+
+存放数据结构，包括user、task等
+
+**user**
+
+用户系统微服务，主要处理用户的Get、Post、Put、Patch、Delete等请求
+
+**……（其他微服务）**
+
+之后，代码结构可以再次调整，将共用代码进行提取，减少代码冗余。
+
+## GoKit CLI使用方法
+
+**GoKit CLI**使用方法可以参考<https://medium.com/@kujtimii.h/creating-a-todo-app-using-gokit-cli-20f066a58e1>
+
+```bash
+# 以用户系统微服务为例
+$ kit n s user
+# 之后定义相关服务（Get、Post等）
+$ kit g s user -w --gorilla
+#-w generate some default service middleware.
+#--gorilla use gorilla/mux instead of the default http handler for the http transport.
+$ go run user/cmd/main.go
+#ts=2019-04-16T09:52:56.5674053Z caller=service.go:78 tracer=none
+#ts=2019-04-16T09:52:56.5714055Z caller=service.go:100 transport=HTTP addr=:8081
+#ts=2019-04-16T09:52:56.5714055Z caller=service.go:134 transport=debug/HTTP addr=:8080
+#端口为8081
+```
+
---
+++ db/data.sql
@@ -0,0 +1,42 @@
+# 创建相应的数据库
+create database if not exists MoneyDodo;
+
+use MoneyDodo;
+drop table user;
+# 用户
+create table if not exists user (
+	id varchar(20) primary key COMMENT '学号',
+    name varchar(20) not null COMMENT '姓名',
+    password varchar(20) not null COMMENT '密码',
+    introduction text COMMENT '个人简介',
+    balance double COMMENT '余额',
+    icon MEDIUMBLOB COMMENT '头像',
+    phone varchar(11) COMMENT '电话号码',
+    creditScore int COMMENT '信用分数',
+    email varchar(20) COMMENT '邮箱'
+);
+
+# 管理员
+create table if not exists admin (
+    name varchar(20) not null COMMENT '姓名',
+    password varchar(20) not null COMMENT '密码'
+);
+
+# 企业
+create table if not exists enterprise (
+    name varchar(20) not null COMMENT '姓名',
+    password varchar(20) not null COMMENT '密码'
+);
+
+# 任务
+create table if not exists task (
+	taskId int AUTO_INCREMENT primary key COMMENT '任务id',
+    taskType varchar(20) not null COMMENT '任务类型',
+    taskFrom varchar(20) not null COMMENT '发布者',
+    recipient text COMMENT '接收者',
+    taskLimit text COMMENT '任务限制',
+    releaseTime text COMMENT '发布时间',
+    cutoffTime text COMMENT '截至时间',
+    rewardAmount double COMMENT '赏金金额',
+    taskStatus varchar(20) COMMENT '任务状态'
+);
