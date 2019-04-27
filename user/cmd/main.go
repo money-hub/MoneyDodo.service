3fd17144fcf859708aa2a03ae1b07e80b1b298f1
liuyh73 <15989067460@163.com>
2019-04-27 19:49:36 +0800

    更新swaggerui

--- user/cmd/main.go
+++ user/cmd/main.go
@@ -1,3 +1,31 @@
+// Copyright 2019 money-hub. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// money-hub MoneyDodo/user
+//
+// This documentation describes example APIs found under https://github.com/ribice/golang-swaggerui-example
+//
+//     Schemes: http
+//     Version: 1.0.0
+//     License: MIT http://opensource.org/licenses/MIT
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Security:
+//     - bearer
+//
+//     SecurityDefinitions:
+//     bearer:
+//          type: apiKey
+//          name: Authorization
+//          in: header
+//
+// swagger:meta
 package main
 
 import (
