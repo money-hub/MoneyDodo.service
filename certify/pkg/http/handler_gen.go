0040b68370b5998397bf3f2d4cf8113db3e7eba6
LXmain <liux276@163.com>
2019-04-28 12:44:43 +0800

    fix data.sql

--- certify/pkg/http/handler_gen.go
+++ certify/pkg/http/handler_gen.go
@@ -2,10 +2,11 @@
 package http
 
 import (
+	http1 "net/http"
+
 	http "github.com/go-kit/kit/transport/http"
 	mux "github.com/gorilla/mux"
 	endpoint "github.com/money-hub/MoneyDodo.service/certify/pkg/endpoint"
-	http1 "net/http"
 )
 
 // NewHTTPHandler returns a handler that makes a set of endpoints available on
