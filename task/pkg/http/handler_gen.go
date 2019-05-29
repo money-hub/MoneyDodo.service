19eaefa6e4bd14e575cd817881bc3505f7e24883
linshk <ggatlsk@gmail.com>
2019-05-29 13:15:51 +0800

    fix bug:task-api 401

--- task/pkg/http/handler_gen.go
+++ task/pkg/http/handler_gen.go
@@ -7,12 +7,14 @@ import (
 	http "github.com/go-kit/kit/transport/http"
 	mux "github.com/gorilla/mux"
 	endpoint "github.com/money-hub/MoneyDodo.service/task/pkg/endpoint"
+	MyJwt "github.com/money-hub/MoneyDodo.service/middleware"
 )
 
 // NewHTTPHandler returns a handler that makes a set of endpoints available on
 // predefined paths.
 func NewHTTPHandler(endpoints endpoint.Endpoints, options map[string][]http.ServerOption) http1.Handler {
 	m := mux.NewRouter()
+	m.Use(MyJwt.GetTokenInfo)
 	makeGetHisReleasedTasksHandler(m, endpoints, options["GetHisReleasedTasks"])
 	makeGetTasksByIDHandler(m, endpoints, options["GetTasksByID"])
 	makeGetHisUnreleasedTasksHandler(m, endpoints, options["GetHisUnreleasedTasks"])
